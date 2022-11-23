354b2536116188848d12201b24f65ad31a054970
julianad <julianaspdimitriadis@gmail.com>
2022-11-23 11:01:04 +0100

    chore: project set-up with Typescript, React-App, Material UI, husky and commitlint

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders app', () => {
+  render(<App />);
+  const text = screen.getByText("I am APP");
+  expect(text).toBeInTheDocument();
+});
+

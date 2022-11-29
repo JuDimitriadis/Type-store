66834ab17105e498f55441eb68303284e1dd6f0e
julianad <julianaspdimitriadis@gmail.com>
2022-11-29 12:56:17 +0100

    style: added theme to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,41 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+      primary: {
+          main: '#05445E',
+          contrastText: '#D4F1F4',
+      },
+      secondary: {
+          main: '#189AB4',
+          contrastText: '#05445E',
+      },
+      background: {
+          paper: "D4F1F4",
+          default: "#189AB4"
+      },
+      text: {
+          primary: '#D4F1F4',
+          secondary:'#05445E'
+      }
+  },
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <App/>
+    </ThemeProvider>
   </React.StrictMode>
 );
 

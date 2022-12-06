4332e1292064019369a94a20e312c89bccf93331
julianad <julianaspdimitriadis@gmail.com>
2022-12-06 13:45:10 +0100

    chore: modify App.tsx, add getCarts Functions to fetch data, and handle res successful or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+interface Error {
+  error: boolean,
+  message: string,
+};
+
 function App() {
+  const [apiError, setApiError] = useState<Error>({error: false, message: ""})
+
+  //Fetching data from the "fakestoreapi"
+  //This function will be called inside the useEffect hook, every time the user load the page 
+  const getCarts = async () => {
+    const response = await fetch('https://fakestoreapi.com/carts')
+    console.log("response", response)
+    // response.ok returns true if the request was successful (status in the range 200-299) and false if it was not successful.
+    if(response.ok === false) {
+      setApiError({
+        error: true,
+        message: `${response.status} - Something went wrong, please try again`
+      })
+      return
+    }
+
+    // status 204 = No Content
+    if(response.status === 204) {
+      setApiError({
+        error: true,
+        message: `No carts available`
+      })
+      return
+    }
+
+    const parse = await response.json();
+
+    //add redux and encapsulation 
+    return;
+  }
+
+  useEffect(()=> {
+    getCarts();
+  }, [])
+
+
+
+
+console.log("apiError", apiError)
   return (
-<h1>I am APP</h1>
+<>
+{apiError.error && <>{apiError.message}</>}
+</>
   );
 }
 

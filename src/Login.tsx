c27240229d78054207aa6281364563679999fe66
julianad <julianaspdimitriadis@gmail.com>
2022-12-16 13:01:38 +0100

    feat: add props, handle setCookis in case of login success

--- src/Login.tsx
+++ src/Login.tsx
@@ -7,6 +7,10 @@ const headers = {'Content-Type':'application/json',
                     'Access-Control-Allow-Origin':'*',
                     'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
 
+  type MyProps = {
+    setCookie: Function
+  }
+  
   type MyState = {
     username: string,
     password: string,
@@ -16,8 +20,8 @@ const headers = {'Content-Type':'application/json',
     }
   };
   
-  export default class Login extends React.Component<any,MyState> {
-    constructor(props: any, state: MyState) {
+  export default class Login extends React.Component<MyProps,MyState> {
+    constructor(props: MyProps, state: MyState) {
         super(props);
         this.state = state;
 
@@ -37,9 +41,15 @@ const headers = {'Content-Type':'application/json',
             }
         )
     };
-    
+
     async handleClick(evt:{preventDefault:Function}) {
         evt.preventDefault();
+        this.setState( { ...this.state,
+            alert: {
+                severity:"",
+                errorMsg:""
+                }
+            })
         if (this.state.username && this.state.password) {
             const response = await fetch('https://fakestoreapi.com/auth/login',{
                 method:'POST',
@@ -49,12 +59,14 @@ const headers = {'Content-Type':'application/json',
                     password: this.state.password
                 })
             });
+
             if (response.ok) {
                 const parse = await response.json();
-                if (parse.token){
-                    // set cookie
-                    // setCookie('Type-Store', {user: this.state.username, auth: true}, { path: '/',maxAge: 1000 * 60 * 60 * 24, secure: true  });
-                    return 
+                if (parse.token) {
+                    this.props.setCookie("TypeStore", {user: "username", auth: true}, { path: '/',maxAge: 1000 * 60 * 60 * 24, secure: true  })
+                    // eslint-disable-next-line no-restricted-globals
+                    location.reload();
+                    return
                 } else {
                     // All success call are supposed to come back with a token property, if this do not happen, 
                     // we have an error, or something change on the API side, developers should be notified. 
@@ -67,39 +79,59 @@ const headers = {'Content-Type':'application/json',
                     //In a real application, I would be logged it in a database, and developers notified by e-mail.
                     console.log("response", response, "parse", parse);
                     return 
-                } 
+                }
             } else {
-            if (response.status === 401) {
-                //401 Unauthorized response status code indicates that the client request has not been completed because it lacks valid authentication credentials for the requested resource
-                //Wrong username and/or password
-                this.setState( { ...this.state,
+                if (response.status === 401) {
+                    //401 Unauthorized response status code indicates that the client request has not been completed because it lacks valid authentication credentials for the requested resource
+                    //Wrong username and/or password
+                    this.setState( { ...this.state,
+                        alert: {
+                            severity:"warning",
+                            errorMsg:"Ops! Wrong username and/or password, please check your data and try again"
+                            }
+                        })
+                    return
+                } else {
+                    //All others api errors (400.., 500.., ...)
+                    this.setState( { ...this.state,
+                        alert: {
+                            severity:"error",
+                            errorMsg:"Ops! Something went wrong, please try again in a few seconds"
+                            }
+                        })
+                    return
+                }
+            }
+        } else {
+             //Blank field validation
+             this.setState( 
+                { ...this.state,
                     alert: {
                         severity:"warning",
-                        errorMsg:"Ops! Wrong username and/or password, please check your data and try again"
-                    }
+                        errorMsg:"Please fill out all fields before continue"
+                    }  
                 })
-                return
-            } else {
-                //All others api errors (400.., 500.., ...)
-                this.setState( { ...this.state,
-                    alert: {
-                        severity:"error",
-                        errorMsg:"Ops! Something went wrong, please try again in a few seconds"
-                    }
-                })
-                return
-            }
-        } } else {
-            //Blank field validation
-            this.setState( { ...this.state,
-                alert: {
-                    severity:"warning",
-                    errorMsg:"Please fill out all fields before continue"
-                }
-                
-            })
+            return
         }
+
     }
+
+
+
+                    // const setCookie = await managingCookies(true, false, this.state.username)
+                    // if(setCookie.auth){
+                    //     // eslint-disable-next-line no-restricted-globals
+                    //     location.reload();
+                    //     return
+                    // } else {
+                    //     this.setState( { ...this.state,
+                    //         alert: {
+                    //             severity:"error",
+                    //             errorMsg:"Ops! Something went wrong, please try again in a few seconds"
+                    //         }
+                    //     })
+                    //     // console.log("setCookie Error", setCookie)
+      
   
     render() {
         return (
@@ -119,3 +151,4 @@ const headers = {'Content-Type':'application/json',
       );
     }
   }
+

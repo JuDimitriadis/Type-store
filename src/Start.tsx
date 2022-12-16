c98ef669460fb7c2525990024bc19d95979feaa7
julianad <julianaspdimitriadis@gmail.com>
2022-12-16 12:52:36 +0100

    feat: new component Start to manage useCookies hook

---
+++ src/Start.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import App from './App';
+import Login from './Login';
+import { useCookies } from 'react-cookie';
+
+
+function Start() {
+    const [cookies, setCookie, removeCookie] = useCookies(["TypeStore"]);
+   
+    return (<>
+        {cookies.TypeStore ? <App removeCookie={removeCookie}></App> : <Login setCookie={setCookie}></Login>}
+    \</>)
+   
+};
+
+export default Start

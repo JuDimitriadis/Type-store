6f8b0413fffac128e3f1a7f9938704f15cddfe17
julianad <julianaspdimitriadis@gmail.com>
2023-01-16 10:51:18 +0100

    test: add Login unit test

--- src/Login.test.tsx
+++ src/Login.test.tsx
@@ -1,3 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Login from './Login';
+import userEvent from "@testing-library/user-event";
+
+describe("When the <Login> component is rendered", () => {
+    it("must have a username and password field, and a Login Button", ()=>{
+        render(<Login setCookie={Function}></Login>)
+        
+        expect(screen.getByRole('textbox')).toHaveAttribute("name", "username");
+        expect(screen.getByTestId('password')).toBeInTheDocument();
+        expect(screen.getByText("Login")).toBeInTheDocument();
+    
+    })
+})
+

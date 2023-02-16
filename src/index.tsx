29f93c40e45add8125cb9e9388b6f2a4269a0730
julianad <julianaspdimitriadis@gmail.com>
2023-02-16 15:18:10 +0100

    style: change theme.background color

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Start from './Start';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Start from "./Start";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-
 
 const theme = createTheme({
   palette: {
-      primary: {
-          main: '#8adcea',
-          contrastText: '#f49d55',
-      },
-      secondary: {
-          main: '#fbb484',
-          contrastText: '#48443f',
-      },
-      background: {
-          paper: "#c2e6e5",
-          default: "transparent"
-      },
-      text: {
-          primary: '#48443f',
-          secondary:'#494440'
-      }
+    primary: {
+      main: "#8adcea",
+      contrastText: "#f49d55",
+    },
+    secondary: {
+      main: "#fbb484",
+      contrastText: "#48443f",
+    },
+    background: {
+      paper: "#transparent",
+      default: "transparent",
+    },
+    text: {
+      primary: "#48443f",
+      secondary: "#494440",
+    },
   },
 });
 
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-        <Start></Start>
+      <Start></Start>
     </ThemeProvider>
   </React.StrictMode>
 );
